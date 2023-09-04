e5c3e1f5f89ee330aa90821864a491384edc1e4a
icekylin <qxw092@student.bham.ac.uk>
2023-09-04 10:22:36 +0800

    update connector

--- src/net/connector.rs
+++ src/net/connector.rs
@@ -1,42 +1,110 @@
 use std::net::SocketAddr;
 use std::process;
 
-use log::error;
+use log::{error, info};
+use tokio::io;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::net::TcpStream;
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::Sender;
+use tokio_util::sync::CancellationToken;
 
-use crate::cli;
 use crate::net::connectable::Connectable;
+use crate::{cli, util};
 
 struct Connector<'a> {
-    cli: &'a cli::Cli,
+    _cli: &'a cli::Cli,
     socket_addr: SocketAddr,
-    socket: Option<TcpStream>,
+    rd: Option<OwnedReadHalf>,
+    wr: Option<OwnedWriteHalf>,
+    tx: Sender<Vec<u8>>,
+    tk: CancellationToken,
 }
 
 pub async fn connect_to_server(cli: &cli::Cli) {
-    Connector::build(cli).await.connect().await;
+    Connector::build(cli).connect().await.listen().await;
 }
 
 impl Connectable for Connector<'_> {}
 
 impl<'a> Connector<'a> {
-    async fn build(cli: &'a cli::Cli) -> Connector<'a> {
+    fn build(cli: &'a cli::Cli) -> Connector<'a> {
+        let (tx, _) = broadcast::channel(16);
+
         Self {
-            cli,
+            _cli: cli,
             socket_addr: Connector::get_socket_addr(cli),
-            socket: None,
+            rd: None,
+            wr: None,
+            tx,
+            tk: CancellationToken::new(),
         }
     }
 
     async fn connect(&mut self) -> &mut Connector<'a> {
-        self.socket = Some(match TcpStream::connect(self.socket_addr).await {
+        let (rd, wr) = match TcpStream::connect(self.socket_addr).await {
             Ok(socket) => socket,
             Err(e) => {
-                error!("Bind to {}: {}. QUITTING", self.socket_addr, e);
+                error!("Connect to {}: {}. QUITTING", self.socket_addr, e);
                 process::exit(exitcode::NOHOST);
+            }
+        }
+        .into_split();
+
+        self.rd = Some(rd);
+        self.wr = Some(wr);
+
+        info!("Connecting to {}.", self.socket_addr);
+
+        self
+    }
+
+    async fn listen(&mut self) {
+        self.spawn_input().spawn_write().read().await;
+
+        info!("Connection from {} closed.", self.socket_addr);
+    }
+
+    fn spawn_input(&mut self) -> &mut Connector<'a> {
+        let tx_clone = self.tx.clone();
+        util::spawn_cancellable_task(&self.tk, async move {
+            let mut buffer = vec![0; 8192];
+            loop {
+                let n = io::stdin().read(&mut buffer).await.unwrap(); // os error
+
+                if n == 0 || tx_clone.send(buffer[..n].to_owned()).is_err() {
+                    break;
+                }
             }
         });
 
         self
+    }
+
+    fn spawn_write(&mut self) -> &mut Connector<'a> {
+        let mut rx = self.tx.subscribe();
+        let mut wr = self.wr.take().unwrap(); // safe
+
+        util::spawn_cancellable_task(&self.tk, async move {
+            while let Ok(msg) = rx.recv().await {
+                if wr.write_all(&msg).await.is_err() {
+                    break;
+                }
+            }
+        });
+
+        self
+    }
+
+    async fn read(&mut self) {
+        io::copy(
+            self.rd.as_mut().unwrap(), // safe
+            &mut io::stdout(),
+        )
+        .await
+        .unwrap(); // os error
+
+        self.tk.cancel();
     }
 }
